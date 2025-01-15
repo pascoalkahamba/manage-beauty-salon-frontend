b2430138c3087210d73a82311ccfe691b70548ac
Pascoal Kahamba <pascoalkahamba25@gmail.com>
2025-01-15 13:59:21 +0100

    feature: I've just created the header and footer components

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,51 @@
+import {
+  IconBrandInstagram,
+  IconBrandTwitter,
+  IconBrandYoutube,
+} from "@tabler/icons-react";
+import { ActionIcon, Anchor, Group } from "@mantine/core";
+import classes from "@/components/Footer/styles.module.css";
+
+const links = [
+  { link: "#", label: "Contact" },
+  { link: "#", label: "Privacy" },
+  { link: "#", label: "Blog" },
+  { link: "#", label: "Store" },
+  { link: "#", label: "Careers" },
+];
+
+export default function Footer() {
+  const items = links.map((link) => (
+    <Anchor
+      c="dimmed"
+      key={link.label}
+      href={link.link}
+      lh={1}
+      onClick={(event) => event.preventDefault()}
+      size="sm"
+    >
+      {link.label}
+    </Anchor>
+  ));
+
+  return (
+    <div className={classes.footer}>
+      <div className={classes.inner}>
+        <h1>Salao de Beleza</h1>
+        <Group className={classes.links}>{items}</Group>
+
+        <Group gap="xs" justify="flex-end" wrap="nowrap">
+          <ActionIcon size="lg" variant="default" radius="xl">
+            <IconBrandTwitter size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" variant="default" radius="xl">
+            <IconBrandYoutube size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" variant="default" radius="xl">
+            <IconBrandInstagram size={18} stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </div>
+    </div>
+  );
+}
---
+++ src/utils/provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+import "@mantine/carousel/styles.css";
+import "@mantine/dates/styles.css";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import AOS from "aos";
+import { useEffect } from "react";
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export default function Providers({ children }: ProvidersProps) {
+  const queryClient = new QueryClient();
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <section>{children}</section>
+    </QueryClientProvider>
+  );
+}
